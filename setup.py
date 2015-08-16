e2997bcd8ef744009853b97515a4270dc67c48bf
Michael Ludwig <mluds@ymail.com>
2015-08-16 19:55:57 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hexapod',
+    description='A high-level interface for the ArcBotics Hexy robot',
+    url='https://github.com/mluds/hexapod',
+    author='Michael Ludwig',
+    author_email='mluds@ymail.com',
+    packages=find_packages(),
+    install_requires=['pyserial']
+)
