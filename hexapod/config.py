71e11c0967f38a1f68005c3edd4e7d34b3ecff9b
Michael Ludwig <mluds@ymail.com>
2015-08-17 23:24:45 -0400

    Updated config.py

--- hexapod/config.py
+++ hexapod/config.py
@@ -1,4 +1,17 @@
 import yaml
-from os.sys import expanduser
+from os.path import expanduser
 
-# TODO: Implement class for cross-platform loading of the user config
+
+def _merge(default, update):
+    pass
+
+
+def _load_config():
+    config = {}
+    with open('config_default.yml') as default:
+        with open(expanduser('~/.hexapod.yml')) as user:
+            config = _merge(default, user)
+    return config
+
+
+config = _load_config()
