e12c5143a37dd4240538aa862feceb33872521c1
Michael Ludwig <mluds@ymail.com>
2015-08-20 20:44:14 -0400

    Fixed user config error

--- hexapod/config.py
+++ hexapod/config.py
@@ -1,9 +1,11 @@
 import lya
 from pkg_resources import resource_stream
-from os.path import expanduser
+import os.path
 
 
 config = lya.AttrDict.from_yaml(
     resource_stream(__name__, 'config_default.yml')
 )
-config.update_yaml(expanduser('~/.hexapod.yml'))
+_user_path = os.path.expanduser('~/.hexapod.yml')
+if os.path.isfile(_user_path):
+    config.update_yaml(_user_path)
